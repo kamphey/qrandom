2057490f5452524532fdd471ce1cc7b27e98a46c
Andrew Kamphey <kamphey@gmail.com>
2016-01-24 12:59:33 -0800

    tweaked animated words
    tweaked the animated words, maybe worked this time to not go off to the
    right.

--- index.php
+++ index.php
@@ -34,14 +34,11 @@ $counter = count($xml->url);
 		<!--End Button-->
 
 		<!--One sentence pitch-->
-		<h2>to discover a <b>random 
-		
-		<div class="rw-words rw-words-2"> 
+		<h2>to discover a <b>random <div class="rw-words rw-words-1"> 
 	<span>Amazing</span>
 	<span>Inspiring</span>
 	<span>Mind-Blowing</span>
-	<span>Funny</span>
-	</div>video</b></h2>
+	<span>Funny</span> </div> video</b></h2>
 		<!--End One sentence pitch-->
 
 		<!--Counter, automatically shows how many websites you have in your urls.xml file-->
